f295ce7571cd8d70f1bac61753dc8c50a40d0c44
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-01 22:38:36 +0200

    Chore: Add navbar component and update font family to inter

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Navbar from "@/components/Navbar";
 export default function Home() {
-  return <main>MERN Blog</main>;
+  return (
+    <div className="px-4 md:px-8 xl:px-16 2xl:px-64">
+      {/* Navbar */}
+      <Navbar />
+      {/* Breadcrump  */}
+      {/* Introduction */}
+      {/* Featured Posts */}
+      {/* Post List */}
+    </div>
+  );
 }
