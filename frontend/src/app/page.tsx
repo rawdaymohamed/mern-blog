04a2296a0497557da19c4f9a3441eedfac24cae6
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-02 19:23:52 +0200

    Feat: Add write button to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
         <span className="text-[#14213D]">Blogs and Articles</span>
       </div>
       {/* Introduction */}
-      <div className="flex justify-between items-center mt-5">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-5 mt-5">
         {/* titles */}
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 md:w-4/5">
           <h1 className="text-2xl md:text-2xl lg:text-3xl font-bold text-gray-800">
             Share your story with the world.
           </h1>
@@ -22,8 +22,18 @@ export default function Home() {
             through the power of blogging.
           </p>
         </div>
-        {/* animated button */}
-        <Link href="/write"></Link>
+
+        <Link
+          href="/write"
+          className="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+          data-text="Write"
+        >
+          <span className="align-middle">W</span>
+          <span className="align-middle">r</span>
+          <span className="align-middle">i</span>
+          <span className="align-middle">t</span>
+          <span className="align-middle">e</span>
+        </Link>
       </div>
       {/* Featured Posts */}
       {/* Post List */}
