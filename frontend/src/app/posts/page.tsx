ecc76c4165f8343ba038b9292e4a067496cc85ef
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-06 02:18:27 +0200

    Feat: Add Posts page frontend with side menu

--- frontend/src/app/posts/page.tsx
+++ frontend/src/app/posts/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+import RecentPosts from "@/components/RecentPosts";
+import SideMenu from "@/components/SideMenu";
+import { useState } from "react";
+
 const Page = () => {
-  return <>Posts</>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="text-gray-800">
+      <h1 className="mt-3 mb-8 text-2xl">Development Blog</h1>
+      <button
+        className="md:hidden bg-blue-800 text-white text-sm px-4 py-2 rounded-2xl mb-4"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {isOpen ? "Close" : "Filter or Search"}
+      </button>
+      <div className="flex flex-col-reverse md:flex-row gap-8">
+        {/* Post List */}
+        <div className="md:w-10/12">
+          <RecentPosts />
+        </div>
+        {/* Side Menu */}
+        <div className={`${isOpen ? "block" : "hidden"} md:block md:w-2/12`}>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
