d9dbee7b7864bdbe2e510298fb66213a7e8054bc
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-12 22:06:51 +0200

    Feat: Add express-validator package and validated user register data

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,19 +1,20 @@
 
 import { Request, Response } from 'express';
 import { User } from "../models/user.model";
+import { validationResult } from 'express-validator';
 
 export const register = async (req: Request, res: Response) => {
-    try {
-        const data = await User.create(req.body);
-        const response = await User.findById(data._id).select("username email");
-        return res.status(201).json({
-            status: "Success",
-            data: response,
-        });
-    } catch (error: any) {
-        return res.status(500).json({
-            status: "Failed",
-            message: error.message || "An error occurred while creating the user",
-        });
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.send({ errors: result.array() })
     }
+
+    const data = await User.create(req.body);
+    const response = await User.findById(data._id).select("username email");
+
+    return res.status(201).json({
+        status: "Success",
+        data: response,
+
+    })
 };
