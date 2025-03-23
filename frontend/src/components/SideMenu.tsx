b550fa6ae197c3b4f945d9337f5a8333ca7eb167
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-23 02:33:44 +0200

    Fix: fix side nav component to add general and all categories on the frontend

--- frontend/src/components/SideMenu.tsx
+++ frontend/src/components/SideMenu.tsx
@@ -53,7 +53,8 @@ const SideMenu = () => {
       <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
       <div className="flex flex-col gap-2 text-sm">
         {[
-          { name: "All", value: "general" },
+          { name: "All", value: "" },
+          { name: "General", value: "General" },
           { name: "Web Design", value: "web-design" },
           { name: "Development", value: "development" },
           { name: "Databases", value: "databases" },
